b412c6b0e781c3f34099915db5ef4c947d772f2a
Daniel Friedle <daniel.friedle@gmail.com>
2017-06-19 16:01:39 +0200

    Create VS project WordList
    Reads the wordlist.txt line by line. Each line is written to the console.

---
+++ WordList/WordList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WordList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string line;
+			var file = new System.IO.StreamReader("wordlist.txt");
+			while ((line = file.ReadLine()) != null)
+			{
+				Console.WriteLine(line);
+			}
+			file.Close();
+			Console.ReadLine();
+		}
+	}
+}
